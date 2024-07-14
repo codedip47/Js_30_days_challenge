d50d9d957b60bee9b646dd0058b00e3d0a7b7b96
codedip47 <dip18052002@gmail.com>
2024-07-14 11:43:38 +0530

    Day 2 completed

---
+++ day_2/tasks.js
@@ -0,0 +1,96 @@
+// ac 1
+    // task 1
+    {
+        let num1 = 5;
+        let num2 = 6;
+        console.log(num1 + num2);
+    }
+    // task 2
+    {
+        let num1 = 15;
+        let num2 = 6;
+        console.log(num1 - num2);
+    }
+    // task 3
+    {
+        let num1 = 5;
+        let num2 = 6;
+        console.log(num1 * num2);
+    }
+    // task 4
+    {
+        let num1 = 17;
+        let num2 = 3;
+        console.log(num1 / num2);
+    }
+    // task 5
+    {
+        let num1 =14;
+        let num2 = 6;
+        console.log(num1 % num2);
+    }
+// ac 2
+    // task 6
+    {
+        let num = 5;
+        num += 10;
+        console.log(num);
+    }
+    // task 7
+    {
+        let num = 5;
+        num -= 10;
+        console.log(num);
+    }
+// ac 3
+    // task 8 , 9
+    {
+        let num1 = 5;
+        let num2 = 6;
+        console.log(5 < 6);
+        console.log(5 > 6);
+        console.log(5 >= 6);
+        console.log(5 <= 6);
+    }
+    // task 10
+    {
+        let num1 = "12";
+        let num2 = 12;
+        console.log(num1 == num2);
+        console.log(num1 === num2);
+    }
+// ac 4
+    // task 11
+    {
+        let num = 5;
+        if(5 > 3 && 5 < 6)
+        {
+            console.log(`${num} is greater than 3 and less than 6`)
+        }
+    }
+    // task 12
+    {
+        let num = 3;
+        if(num === 2 || num === 3)
+        {
+            console.log(`Number entered is either 3 or 2`);
+        }
+    }
+    // task 13
+    {
+        let val = false;
+        if(!val)
+        {
+            console.log(`It changes true to false , likewise false to true`);
+        }
+    }
+// ac 5
+    // task 14
+    {
+        let num = 5;
+        num >= 0 ? console.log(`positive`) : console.log(`Negative`);
+        num = -3
+        num >= 0 ? console.log(`Positive`) : console.log(`Negative`);
+    }
+
+/* All feature requests are here as well */
